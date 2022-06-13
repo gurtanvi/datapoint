606031e199f18b9b27f9721e92288a6edcf884ce
Gurleen Kaur <gurleenkaur@MacBook-Air-von-Administrator.local>
2022-06-13 15:43:42 +0200

    Sidekiq

--- Gemfile
+++ Gemfile
@@ -27,8 +27,13 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+gem 'pg'
+
+
+gem 'sidekiq'
+gem 'sidekiq-scheduler'
+
 group :production do
-  gem 'pg'
 end
 
 # Use Redis adapter to run Action Cable in production
@@ -61,6 +66,7 @@ group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
 
+  gem 'pry'
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
--- app/models/dtp_minute.rb
+++ app/models/dtp_minute.rb
@@ -1,2 +1,23 @@
+require 'sidekiq-scheduler'
+
 class DtpMinute < ApplicationRecord
+    
+    include Sidekiq::Worker
+    before_create :retrieve_value 
+    
+    def retrieve_value
+        require 'uri'
+        require 'net/http'
+
+        uri = URI("https://itsmybike-staging.cloud/api/monitoring/datapoints_last_minute")
+        req = Net::HTTP::Get.new(uri)
+        req['Authorization'] = "Token token=8fe569ac"
+        req['Content-Type'] = "application/json"
+
+
+        res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+            http.request(req)
+        end
+        self.amount = res.body.to_i
+    end
 end
